00ed64fe4e9be6368168f30cbd7c6cbf4a30e5f0
Marksman7 <56178417+Marksman7@users.noreply.github.com>
2023-03-16 22:10:58 -0500

    added_script_for_multiple_characters
    -Novel_moduel_multi is create for multiple characters

---
+++ Novel_Love_Beating/Novel_Love_Beat/Assets/Scripts/Novel_Aspects/Novel_Multiple.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Novel_Multiple : MonoBehaviour
+{
+    public string[] characters;
+    public string[] dialogue_character;
+
+    private GameObject get_background;
+    private Image Background;
+    private Image[] character_art = { null, null, null, null, null};
+    public Sprite[] insert_art_character;
+    public Sprite insert_art_background;
+    private TextMeshProUGUI dialogue_box;
+    private TextMeshProUGUI character_name_box;
+
+    private GameObject get_dialogue_box;
+    private GameObject get_character_name_box;
+
+    private GameObject[] character_art_reteve = { null, null};
+
+    private int character_number = 0;
+    private int dialogue_counter = 0;
+
+    int get_character_num = 0;
+
+    string temp_string = "";
+
+    void Start()
+    {
+        get_background = GameObject.FindGameObjectWithTag("background");
+        character_art_reteve[0] = GameObject.FindGameObjectWithTag("left_char_image");
+        character_art_reteve[1] = GameObject.FindGameObjectWithTag("Right_char_image");
+
+        get_dialogue_box = GameObject.FindGameObjectWithTag("dialogue_box");
+
+        get_character_name_box = GameObject.FindGameObjectWithTag("character_name_box");
+
+        Background = get_background.GetComponent<Image>();
+
+        character_art[0] = character_art_reteve[0].GetComponent<Image>();
+        character_art[1] = character_art_reteve[1].GetComponent<Image>();
+
+        dialogue_box = get_dialogue_box.GetComponent<TextMeshProUGUI>();
+
+        character_name_box = get_character_name_box.GetComponent<TextMeshProUGUI>();
+
+        /*int im_char = 0;
+        foreach(Image im in character_art)
+        {
+
+            if(im_char >= insert_art_character.Length)
+            {
+                im_char = 0;
+            }
+
+            im.sprite = insert_art_character[im_char];
+            im_char = im_char + 1;
+            
+        }*/
+        character_art[0].sprite = insert_art_character[0];
+        character_art[1].sprite = insert_art_character[0];
+        
+
+
+        Background.sprite = insert_art_background;
+
+        if(dialogue_box == null)
+        {
+            Debug.Log("There is no dialogue_box connected");
+        }
+
+        Start_dialogue_line();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Next_dialogue_line();
+        }
+    }
+
+    void Start_dialogue_line()
+    {
+        /*if(characters[0] != null)
+        {
+            //Debug.Log("notpe");
+        }*/
+        dialogue_box.text = dialogue_character[0];//dialogue_counter
+        character_name_box.text = characters[character_number];//character_number
+
+        Next_character_art_image();
+    }
+
+    void Next_dialogue_line()
+    {
+        dialogue_counter = dialogue_counter + 1;
+
+        if (dialogue_counter >= dialogue_character.Length)
+        {
+            ActivateChoices();
+        }
+        else
+        {
+            dialogue_box.text = dialogue_character[dialogue_counter];
+            /*int*/
+            get_character_num = Int32.Parse(characters[character_number][0].ToString());
+
+            /*string*/
+            character_name_box.text = characters[Int32.Parse(characters[character_number][0].ToString())];
+            temp_string = characters[character_number];
+            temp_string.Remove(0, 1);
+            character_name_box.text = temp_string;
+            Next_character_line();
+        }
+
+    }
+
+    void Next_character_line()
+    {
+
+        character_number = character_number + 1;
+        if (character_number >= characters.Length)
+        {
+            character_number = 0;
+            
+            Next_character_art_image();
+        }
+        else
+        {
+            Next_character_art_image();
+        }
+    }
+
+    void Next_character_art_image()
+    {
+        
+        if(character_number == 0)
+        {
+            character_art[0].GetComponent<Image>().color = new Color32(255, 255, 225, 75);
+            character_art[1].GetComponent<Image>().color = new Color32(255, 255, 225, 225);
+        }
+        else
+        {
+            character_art[1].GetComponent<Image>().color = new Color32(255, 255, 225, 75);
+            character_art[0].GetComponent<Image>().color = new Color32(255, 255, 225, 225);
+        }
+
+    }
+
+    void ActivateChoices()
+    {
+        this.transform.GetComponent<Choice>().activated();
+    }
+}
