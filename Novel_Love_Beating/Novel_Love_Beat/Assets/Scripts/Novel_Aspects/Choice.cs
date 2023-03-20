716e3a2e92785fda8944f92c450573af5e7e80ba
Marksman7 <56178417+Marksman7@users.noreply.github.com>
2023-03-20 02:30:36 -0500

    Added_no_options_next_scne_and_muiple_Different_gotos
    -Choice (script)-if there are no options then it will go to the next scene
    -Choice- 5 different moduals to chosse to get to another branch
    -prefabs updated-

--- Novel_Love_Beating/Novel_Love_Beat/Assets/Scripts/Novel_Aspects/Choice.cs
+++ Novel_Love_Beating/Novel_Love_Beat/Assets/Scripts/Novel_Aspects/Choice.cs
@@ -27,7 +27,7 @@ public class Choice : MonoBehaviour
 
     private int next_scren_num = -1;
 
-    public GameObject next_node_set_active;
+    public GameObject[] next_node_set_active;
 
     private float wait = .01f;
 
@@ -98,8 +98,30 @@ public class Choice : MonoBehaviour
 
     public void activated()
     {
+
+
         player_choices.SetActive(true);
+
+        int i = 0;
+
+        foreach(GameObject gt in get_choices)
+        {
+            if(gt.activeSelf== true)
+            {
+                i = i + 1;
+            }
+        }
+
+
+        if(i <= 0)
+        {
+            nono();
+
+        }
+        else
+        {
         Create_scenes();
+        }
     }
 
     private void Create_scenes()
@@ -121,38 +143,40 @@ public class Choice : MonoBehaviour
 
     public void Button00_click()
     {
-        Nextscene(choices_scene[0], character_number[0], character_points[0]);
+        Nextscene(choices_scene[0], character_number[0], character_points[0], 0);
     }
 
     public void Button01_click()
     {
-        Nextscene(choices_scene[1], character_number[1], character_points[1]);
+        Nextscene(choices_scene[1], character_number[1], character_points[1], 1);
     }
 
     public void Button02_click()
     {
-        Nextscene(choices_scene[2], character_number[2], character_points[2]);
+        Nextscene(choices_scene[2], character_number[2], character_points[2], 2);
     }
 
     public void Button03_click()
     {
         
-        Nextscene(choices_scene[3], character_number[3], character_points[3]);
+        Nextscene(choices_scene[3], character_number[3], character_points[3], 3);
     }
 
     public void Button04_click()
     {
-        Nextscene(choices_scene[4], character_number[4], character_points[4]);
+        Nextscene(choices_scene[4], character_number[4], character_points[4], 4);
     }
 
     private void nono()
     {
-        this.next_node_set_active.SetActive(true);
+        active_choices();
+        this.next_node_set_active[0].SetActive(true);
+        player_choices.GetComponent<button_Choice>().get_new_moduel(wait);
         this.gameObject.SetActive(false);
         
     }
 
-    private void Nextscene(int scene_num, int char_num, int char_point)
+    private void Nextscene(int scene_num, int char_num, int char_point, int next_modual_num)
     {
         if (scene_num == next_scren_num)
         {
@@ -160,7 +184,14 @@ public class Choice : MonoBehaviour
             //Debug.Log(next_node_set_active);
             //Debug.Log(this.gameObject);
             active_choices();
-            this.next_node_set_active.SetActive(true);
+            if (next_node_set_active[next_modual_num] != null)
+            {
+                this.next_node_set_active[next_modual_num].SetActive(true);
+            }
+            else
+            {
+                Debug.Log("forgot to add a modual in the next_node_set_active array, (located in the modual under choice");
+            }
             player_choices.GetComponent<button_Choice>().get_new_moduel(wait);
             hold_points.GetComponent<Point_Holder>().update_points(char_num, char_point);
             this.gameObject.SetActive(false);
